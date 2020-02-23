56bb6a625224bcb6caef8ae17bb0f0904a06f169
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-02-22 17:53:07 -0800

    moved protected endpoints to new file

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -11,11 +11,6 @@ describe('Articles Endpoints', function() {
     testComments,
   } = helpers.makeArticlesFixtures()
 
-  // function makeAuthHeader(user) {
-  //   const token = Buffer.from(`${user.user_name}:${user.password}`).toString('base64')
-  //   return `Basic ${token}`
-  // }
-
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
@@ -29,62 +24,6 @@ describe('Articles Endpoints', function() {
   before('cleanup', () => helpers.cleanTables(db))
 
   afterEach('cleanup', () => helpers.cleanTables(db))
-
-  describe(`Protected endpoints`, () => {
-    beforeEach('insert articles', () =>
-      helpers.seedArticlesTables(
-        db,
-        testUsers,
-        testArticles,
-        testComments
-      )
-    )
-
-    const protectedEndpoints = [
-      {
-        name: 'GET /api/articles/:article_id',
-        path: '/api/articles/1'
-      },
-      {
-        name: 'GET /api/articles/:article_id/comments',
-        path: '/api/articles/1/comments'
-      }
-    ]
-
-    protectedEndpoints.forEach(endpoint => {
-      describe(endpoint.name, () => {
-        it(`responds with 401 'Missing basic token' when no basic token`, () => {
-          return supertest(app)
-            .get(endpoint.path)
-            .expect(401, { error: `Missing basic token` })
-        })
-
-        it(`responds 401 'Unauthorized request' when no credentials in token`, () => {
-          const userNoCreds = { user_name: '', password: '' }
-          return supertest(app)
-            .get(endpoint.path)
-            .set('Authorization', helpers.makeAuthHeader(userNoCreds))
-            .expect(401, { error: `Unauthorized request` })
-        })
-
-        it(`responds 401 'Unauthorized request' when invalid user`, () => {
-          const userInvalidCreds = { user_name: 'user-not', password: 'existy' }
-          return supertest(app)
-            .get(endpoint.path)
-            .set('Authorization', helpers.makeAuthHeader(userInvalidCreds))
-            .expect(401, { error: `Unauthorized request` })
-        })
-
-        it(`responds 401 'Unauthorized request' when invalid password`, () => {
-          const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' }
-          return supertest(app)
-            .get(endpoint.path)
-            .set('Authorization', helpers.makeAuthHeader(userInvalidPass))
-            .expect(401, { error: `Unauthorized request` })
-        })
-      })
-    })
-  })
 
   describe(`GET /api/articles`, () => {
     context(`Given no articles`, () => {
