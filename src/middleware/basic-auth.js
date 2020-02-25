ac885c51e65717c3d6aa7f0f226d425486434228
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-02-25 12:52:18 -0800

    using hashed passwords for data protection

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const AuthService = require('../auth/auth-service')
 
 function requireAuth(req, res, next) {
@@ -24,12 +25,18 @@ function requireAuth(req, res, next) {
     )
       .then(user => {
         //check password
-        if (!user || user.password !== tokenPassword) {
+        if (!user) {
           return res.status(401).json({ error: 'Unauthorized request' })
         }
 
-        req.user = user
-        next()
+        return bcrypt.compare(tokenPassword, user.password)
+          .then(passwordsMatch => {
+            if (!passwordsMatch) {
+              return res.status(401).json({ error: 'Unauthorized request' })
+            }
+            req.user = user
+            next()
+          })
       })
       .catch(next)
   }
